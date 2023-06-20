6872570137b6c630b6ac955e3cc8355f0dcf422e
Swati Rajendra <swatirajendra@Swatis-MacBook-Pro-2.local>
2023-06-20 19:45:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
+import { useEffect } from "react";
 import './App.css';
+import usePersistState from "./persistState";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function Counter() {
+
+  const [playerScore1,setPlayerScore1] = usePersistState("playerScore1",0);
+  const [playerScore2,setPlayerScore2] = usePersistState("playerScore2",0);
+  const [disabled,setDisabled] = useState(false);
+
+  return(
+    <div id='main'>
+      <div id='container'>
+        <div id='player-button-div'>
+          <button id='player1' onClick={incrementScore1} disabled={disabled}>Player-1</button>
+          <button id='player2' onClick={incrementScore2} disabled={disabled}>Player-2</button>
+        </div>
+        <div id='score-div'>
+          <p id='score1'>Player-1 Score : {playerScore1}</p>
+          <p id='score2'>Player-2 Score: {playerScore2}</p>
+        </div>
+      </div>
     </div>
-  );
+  )
+
+  function incrementScore1() {
+    if(playerScore1 >= 10 || playerScore2 >= 10) {
+      setDisabled(true);
+    }
+    else{
+      setPlayerScore1(playerScore1+1);
+    }
+  }
+
+  function incrementScore2() {
+    if(playerScore1 >= 10 || playerScore2 >= 10) {
+      setDisabled(true);
+    }
+    else{
+      setPlayerScore2(playerScore2+1);
+    }
+  }
 }
 
-export default App;
+export default Counter;
---
+++ src/persistState.js
@@ -0,0 +1,23 @@
+import { useState, useEffect} from "react";
+
+function getScore(key,defaultScore) {
+    const saved = localStorage.getItem(key);
+    const parsedValue = JSON.parse(saved);
+    return parsedValue || defaultScore;
+}
+
+const usePersistState = (key,defaultScore) => {
+    const [score,setScore] = useState(() => {
+        return getScore(key,defaultScore);
+    });
+
+
+useEffect(()=> {
+    localStorage.setItem(key,score);
+},[key,score]);
+
+return [score,setScore];
+
+}
+
+export default usePersistState;
